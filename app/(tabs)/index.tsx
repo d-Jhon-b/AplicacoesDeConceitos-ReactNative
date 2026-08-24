2f247d656b2f795cd76f67a4b0c4b07c12eb35a1
Luizhenrique2024 <165054824+Luizhenrique2024@users.noreply.github.com>
2026-08-24 20:10:22 -0300

    feat(teste)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,19 @@
 import { StyleSheet,View, Text } from 'react-native';
 import Contador from '../src/contador'
+import Comp from '../src/contador'
+import { useEffect, useState } from 'react';
+
+
+const Pisca=Comp=>{
+  const [isShowingText, setIsShowingText]=useState(true)
+  useEffect(()=>{
+    const toggle = setInterval(() => {
+        setIsShowingText(!isShowingText)
+    }, 1000);
+  })
+
+}
+
 
 export default function TabOneScreen() {
   return(
@@ -12,5 +26,5 @@ export default function TabOneScreen() {
 }
 
 const styles = StyleSheet.create({
-  container:{flexGrow:1,gap:10,justifyContent:'center',alignItems:'center',backgroundColor:"#1532c181c"}
+  container:{flexGrow:1,gap:10,justifyContent:'center',alignItems:'center',backgroundColor:"#1532c181"}
 });
