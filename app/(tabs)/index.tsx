1922181bfc7980b0241c0fa43b4c8fd641bbe0d3
Luizhenrique2024 <165054824+Luizhenrique2024@users.noreply.github.com>
2026-08-24 20:42:09 -0300

    news

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,51 +1,41 @@
-import { StyleSheet,View, Text } from 'react-native';
+import { StyleSheet,View, Text,Button } from 'react-native';
 import Contador from '../src/contador'
 import Comp from '../src/contador'
-import { useEffect, useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 
 
-const Pisca=Comp=>{
-  const [isShowingText, setIsShowingText]=useState(true)
-  useEffect(()=>{
-    const toggle = setInterval(() => {
-        setIsShowingText(!isShowingText)
-    }, 1000);
+const MudarCor =()=>{
+  const [color, setColor]=useState('blue')
 
-    return ()=>{clearInterval(toggle)}
-    
-  })
-  if(!isShowingText){
-    return null   
+  const changeColor=()=>{
+    setColor((prevColor)=>(prevColor ==='blue'? 'green':'blue'))
   }
 
-  return <Text>{Comp.Text}</Text>
-
-}
-
-const piscaApp=()=>{
-  return(
-    <View>
-      <Pisca text='Vamos piscar'/>
-      <Pisca text='SIM, PISCANDO É BOM'/>
-    </View>
+  return (
+      <View style={(styles.quadrado, {backgroundColor:'red', borderWidth:3,borderColor:'black'})}>
+        <Button onPress={changeColor} title='Mudar cor'/>
+      </View>
   )
 }
-
 
 export default function TabOneScreen() {
   return(
     <View style={styles.container}>
+      <View style={styles.quadrado}>
+          <MudarCor/> 
 
-      <Contador inicial={100}/>
+      </View>
+
+      {/* <Contador inicial={100}/> */}
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container:{flexGrow:1,gap:10,justifyContent:'center',alignItems:'center',backgroundColor:"#1532c181"}
+  container:{flex:1,gap:5,justifyContent:'center',alignItems:'center',backgroundColor:"#fff"}
   ,tela1:{
     flexGrow:1, backgroundColor:"green", alignItems:"center",justifyContent:"center"
-  }
-
-
+  },
+  quadrado:{width:100,height:100,marginBottom:20}
 });
